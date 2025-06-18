986a85961ac2bde06a81fac6f9346ef1ae7d7c0e
lrnzgll <lorenzo.galli@usemast.com>
2025-06-18 14:26:51 +0100

    add dockerfile

--- game.rb
+++ game.rb
@@ -12,15 +12,13 @@ if __FILE__ == $PROGRAM_NAME
 
   # Use the Heroku server URL by default, or localhost for testing
   # Set the LOCAL_SERVER environment variable to use localhost
-       server_ip = "localhost"
-      port = 8080
-  #  if ENV['LOCAL_SERVER']
-  #  server_ip = "localhost"
-  #  port = 8080
-  #else
-  #  server_ip = "subprimeshowdown-70e905ab4841.herokuapp.com"
-  #  port = 443  # Use HTTPS port for Heroku
-  #end
+    if ENV['LOCAL_SERVER']
+    server_ip = "localhost"
+    port = 8080
+  else
+    server_ip = "subprimeshowdown-70e905ab4841.herokuapp.com"
+    port = 443  # Use HTTPS port for Heroku
+  end
 
   puts "Connecting to server at #{server_ip}:#{port}..."
 
